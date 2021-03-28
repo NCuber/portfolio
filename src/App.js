fe3d49dbe4a863e43c2355519a16e297792a96f0
NCuber <ac020211@naver.com>
2021-03-29 02:23:34 +0900

    practice title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Tech from "./components/Tech";
+import Introduction from "./components/Introduction";
+import MainProject from "./components/MainProject";
+import Contact from "./components/Contact";
+import Title from "./components/Title";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <Title />
+      <Introduction />
+      <Tech />
+      <MainProject />
+      <Contact />
     </div>
   );
+  // 바로가기 버튼 만들기 . 
+  // 버튼 누를시 상단에서 메뉴바 생성.
+  // 각 단 마다 바로가기 버튼을 만들어서 이동.
+  // 위로가기 버튼 생성.
 }
 
 export default App;
