7ff07cfa1388505a378e14d4e2f0cb79d33caa78
NCuber <ac020211@naver.com>
2021-07-19 22:56:07 +0900

    Update Techjs

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import {useSpring, animated} from "react-spring";
+
+{/* --------- 스타일 선언부 --------- */}
 
 const Main = styled.div`
   width: 100%;
@@ -11,7 +14,13 @@ const Main = styled.div`
 }
 `;
 
-
+const Title = styled.div`
+  font-size: 40pt;
+  text-decoration: underline;
+  font-weight: bold;
+  letter-spacing: 3px;
+  grid-column: 1/3;
+`;
 
 const List = styled.div`
   display: grid;
@@ -24,38 +33,101 @@ const List = styled.div`
 
 `;
 
-const Title = styled.div`
-  font-size: 40pt;
-  text-decoration: underline;
-  font-weight: bold;
-  letter-spacing: 3px;
-  grid-column: 1/3;
+const Circle = styled(animated.div)`
 
-  // text-shadow: 2px 2px;
-  // word-spacing: 단어 사이 간격
-  // text-align : 정렬
-  // line-height: 줄간격
-`;
-
-const One1 = styled.div`
-  border-style: solid;
+  position: relative;
   display: flex;
-  grid-row: 2/4;
-
+  width: 200px;
+  height:200px;
+  border-radius: 50%;
 `;
+
+const Incircle = styled.div`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  text-align:center;
+  line-height: 160px;
+  width:160px;
+  height:160px;
+  overflow:hidden;
+  border-radius: 50%;
+  background: skyblue;
+`;
+
 const One = styled.div`
   border-style: solid;
   display: flex;
 `;
 
+
+
+
 const Tech = () => {
+  {/* --------- 변수 선언부 --------- */}
+
+  const {dono} = useSpring({
+    dono: 100,
+    from: { dono:0 },
+    config: 	{ mass: 1, tension: 150, friction: 70 },
+  });
+
+  {/* --------- 리턴 정의부 --------- */}
   return (
     <Main>
       <List>
         <Title>My Skill</Title>
-        <One1></One1>
-        <One></One>
-        <One></One>
+        <One style={{gridRow : '2/4'}}>
+          <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+
+          <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+
+          <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+
+          <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+
+          <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+
+          <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+        </One>
+        <One>
+        <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+          <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+        </One>
+        <One>
+        <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+          <Circle style={{background: dono.to(dono => `conic-gradient(blue ${dono}% , white ${dono}%)`)}}>
+            <Incircle>
+            </Incircle>
+          </Circle>
+        </One>
       </List>
     </Main>
   );
